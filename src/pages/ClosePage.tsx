12dbe19db5cf77384415f667a6ff57e1736aa52d
Georgii <panovgeorgij819@gmail.com>
2023-09-24 23:07:09 +0500

    add new pages and components

---
+++ src/pages/ClosePage.tsx
@@ -0,0 +1,5 @@
+export const ClosePage = () => {
+    return (
+        <div className="btn_close_page" onClick={() => {window.history.back()}}></div>
+    )
+}
---
+++ src/pages/suport_home.tsx
@@ -0,0 +1,5 @@
+export const SupportHome = () => {
+    return (
+        <h1>Support Home</h1>
+    )
+}

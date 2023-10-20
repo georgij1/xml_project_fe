656eb9a1df7b04e5a8122d59c0f33918efa01f47
Georgii <panovgeorgij819@gmail.com>
2023-10-20 13:23:27 +0500

    refactoring code

--- src/components/message/Logout.tsx
+++ src/components/message/Logout.tsx
@@ -4,11 +4,8 @@ import {useState} from "react";
 export const Logout = () => {
     const [time, setTime] = useState('')
     const [menu, setMenu] = useState<boolean>(false)
-
     const choose_time=localStorage.getItem('MessageLogout')
-
-    // @ts-ignore
-    const interval = (countDown) => {
+    const interval = (countDown : any) => {
         const x = setInterval(() => {
             const now = new Date().getTime()
             const distance = countDown-now
@@ -25,89 +22,18 @@ export const Logout = () => {
     }
 
     const count_call = () => {
-        if (new Date().getMonth()+1===1) {
-            const Day = new Date().getDate()
-            const year = new Date().getFullYear()
-            const countDown = new Date("January"+Day+", "+year+" "+choose_time).getTime()
-            interval(countDown)
-        }
-
-        else if (new Date().getMonth()+1===2) {
-            const Day = new Date().getDate()
-            const year = new Date().getFullYear()
-            const countDown = new Date("February"+ Day+", "+year+" "+choose_time).getTime()
-            interval(countDown)
-        }
-
-        else if (new Date().getMonth()+1===3) {
-            const Day = new Date().getDate()
-            const year = new Date().getFullYear()
-            const countDown = new Date("March"+ Day+", "+year+" "+choose_time).getTime()
-            interval(countDown)
-        }
-
-        else if (new Date().getMonth()+1===4) {
-            const Day = new Date().getDate()
-            const year = new Date().getFullYear()
-            const countDown = new Date("April"+ Day+", "+year+" "+choose_time).getTime()
-            interval(countDown)
-        }
-
-        else if (new Date().getMonth()+1===5) {
-            const Day = new Date().getDate()
-            const year = new Date().getFullYear()
-            const countDown = new Date("May"+ Day+", "+year+" "+choose_time).getTime()
-            interval(countDown)
-        }
-
-        else if (new Date().getMonth()+1===6) {
-            const Day = new Date().getDate()
-            const year = new Date().getFullYear()
-            const countDown = new Date("June"+ Day+", "+year+" "+choose_time).getTime()
-            interval(countDown)
-        }
-
-        else if (new Date().getMonth()+1===7) {
-            const Day = new Date().getDate()
-            const year = new Date().getFullYear()
-            const countDown = new Date("July"+ Day+", "+year+" "+choose_time).getTime()
-            interval(countDown)
-        }
-
-        else if (new Date().getMonth()+1===8) {
-            const Day = new Date().getDate()
-            const year = new Date().getFullYear()
-            const countDown = new Date("August"+ Day+", "+year+" "+choose_time).getTime()
-            interval(countDown)
-        }
-
-        else if (new Date().getMonth()+1===9) {
-            const Day = new Date().getDate()
-            const year = new Date().getFullYear()
-            const countDown = new Date("September"+ Day+", "+year+" "+choose_time).getTime()
-            interval(countDown)
-        }
-
-        else if (new Date().getMonth()+1===10) {
-            const Day = new Date().getDate()
-            const year = new Date().getFullYear()
-            const countDown = new Date("October"+ Day+", "+year+" "+choose_time).getTime()
-            interval(countDown)
-        }
-
-        else if (new Date().getMonth()+1===11) {
-            const Day = new Date().getDate()
-            const year = new Date().getFullYear()
-            const countDown = new Date("November"+ Day+", "+year+" "+choose_time).getTime()
-            interval(countDown)
-        }
-
-        else if (new Date().getMonth()+1===11) {
-            const Day = new Date().getDate()
-            const year = new Date().getFullYear()
-            const countDown = new Date("December"+ Day+", "+year+" "+choose_time).getTime()
-            interval(countDown)
-        }
+        if (new Date().getMonth()+1===1) interval(new Date("January"+new Date().getDate()+", "+new Date().getFullYear()+" "+choose_time).getTime())
+        else if (new Date().getMonth()+1===2) interval(new Date("February"+ new Date().getDate()+", "+new Date().getFullYear()+" "+choose_time).getTime())
+        else if (new Date().getMonth()+1===3) interval(new Date("March"+ new Date().getDate()+", "+new Date().getFullYear()+" "+choose_time).getTime())
+        else if (new Date().getMonth()+1===4) interval(new Date("April"+ new Date().getDate()+", "+new Date().getFullYear()+" "+choose_time).getTime())
+        else if (new Date().getMonth()+1===5) interval(new Date("May"+ new Date().getDate()+", "+new Date().getFullYear()+" "+choose_time).getTime())
+        else if (new Date().getMonth()+1===6) interval(new Date("June"+ new Date().getDate()+", "+new Date().getFullYear()+" "+choose_time).getTime())
+        else if (new Date().getMonth()+1===7) interval(new Date("July"+ new Date().getDate()+", "+new Date().getFullYear()+" "+choose_time).getTime())
+        else if (new Date().getMonth()+1===8) interval(new Date("August"+ new Date().getDate()+", "+new Date().getFullYear()+" "+choose_time).getTime())
+        else if (new Date().getMonth()+1===9) interval(new Date("September"+ new Date().getDate()+", "+new Date().getFullYear()+" "+choose_time).getTime())
+        else if (new Date().getMonth()+1===10) interval(new Date("October"+ new Date().getDate()+", "+new Date().getFullYear()+" "+choose_time).getTime())
+        else if (new Date().getMonth()+1===11) interval(new Date("November"+ new Date().getDate()+", "+new Date().getFullYear()+" "+choose_time).getTime())
+        else if (new Date().getMonth()+1===11) interval(new Date("December"+ new Date().getDate()+", "+new Date().getFullYear()+" "+choose_time).getTime())
     }
 
     count_call()
@@ -115,20 +41,20 @@ export const Logout = () => {
     const logout = () => window.open(`/logout`, '_self')
 
     const close_timer_logout = () => {
-        console.log('click close timer logout')
         setMenu(true)
+        localStorage.setItem('menu', String(true))
     }
 
     const open_timer_logout = () => {
-        console.log('click open timer logout')
         setMenu(false)
+        localStorage.setItem('menu', String(false))
     }
 
     if (localStorage.getItem('theme')) {
         return (
             <>
                 {
-                    menu ? <>
+                    menu || localStorage.getItem('menu') === String(true) ? <>
                         <div>
                             <div className="fixed bottom-10 right-5 bg-slate-500 z-50 p-5
                             rounded-xl cursor-pointer shadow-lg shadow-cyan-500/50
@@ -154,20 +80,20 @@ export const Logout = () => {
         return (
             <>
                 {
-                    menu ? <>
+                    menu || localStorage.getItem('menu') === String(true) ? <>
                         <div>
                             <div className="fixed bottom-10 right-5 bg-slate-500 z-50 p-5
                             rounded-xl cursor-pointer shadow-lg shadow-cyan-500/50
-                            hover:shadow-none" onClick={open_timer_logout}>Открыть</div>
+                            hover:shadow-none text-white" onClick={open_timer_logout}>Открыть</div>
                         </div>
                     </> : <>
                         <div>
                             <div className="fixed bottom-10 right-20 mr-32 bg-slate-500 z-50 p-5
                             rounded-xl cursor-pointer shadow-lg shadow-cyan-500/50
-                            hover:shadow-none" onClick={close_timer_logout}>Закрыть</div>
-                            <div className="fixed bottom-10 right-5 bg-slate-800 z-50 rounded-xl">
-                                <div onClick={logout} className="p-5 bg-red-400 m-5 rounded-xl cursor-pointer shadow-lg shadow-cyan-500/50 hover:shadow-none">Выйти</div>
-                                <div className="p-5 bg-slate-400 m-5 rounded-xl cursor-pointer shadow-lg shadow-cyan-500/50 hover:shadow-none">{''+time}</div>
+                            hover:shadow-none text-white" onClick={close_timer_logout}>Закрыть</div>
+                            <div className="fixed bottom-10 right-5 bg-slate-800 z-50 rounded-xl text-white">
+                                <div onClick={logout} className=" text-white p-5 bg-red-400 m-5 rounded-xl cursor-pointer shadow-lg shadow-cyan-500/50 hover:shadow-none">Выйти</div>
+                                <div className=" text-white p-5 bg-slate-400 m-5 rounded-xl cursor-pointer shadow-lg shadow-cyan-500/50 hover:shadow-none">{''+time}</div>
                             </div>
                         </div>
                     </>

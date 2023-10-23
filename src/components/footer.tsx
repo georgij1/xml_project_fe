8d4e851cbd133caab538c98a636a35c7a8570dae
Georgii <panovgeorgij819@gmail.com>
2023-10-24 00:30:33 +0500

    fix bug with footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,7 +2,7 @@ export const Footer = () => {
     const data = new Date()
     const now_date = data.getFullYear()
     return (
-        <div className="footer">
+        <div className="footer fixed bottom-0">
             <div>{now_date} г. Все права защищены &copy;</div>
         </div>
     )
